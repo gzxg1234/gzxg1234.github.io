a79636f6a8be095b245fb0166c3cc80cdf191234
chenrong <sanronjun@163.com>
2018-11-09 11:16:12 +0800

    a

--- cordovademo/platform_www/plugins/com.cedarhd.WebPlugin/www/WebPlugin.js
+++
@@ -1,8 +0,0 @@
-cordova.define("com.cedarhd.WebPlugin.WebPlugin", function(require, exports, module) {
-var exec = require('cordova/exec');
-
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'WebPlugin', 'coolMethod', [arg0]);
-};
-
-});
